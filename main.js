f7623405468abd2cf520c5fadb3fd0a95390c2d0
QuackDuster <jjimenez@ucol.mx>
2020-02-21 20:09:49 -0600

    Clase Fecha terminada

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import Fecha from "./fecha.js"
 import Tiempo from "./tiempo.js"
 export default class Main {
 
@@ -11,7 +12,21 @@ export default class Main {
         console.log(horas.getFormato12())
         console.log(horas.getFormato24())
     }
+
+    pruebaFecha(){
+        let fecha = new Fecha(5,4,2017)
+        console.log("----------Fecha---------")
+        console.log(fecha.getAños())
+        console.log(fecha.getMeses())
+        console.log(fecha.getSemanas())
+        console.log(fecha.getDias())
+        console.log(fecha.getFecha())
+        console.log(fecha.getDiaFecha())
+    }
+
+    
 }
 
 let tester = new Main()
 tester.pruebaTiempo();
+tester.pruebaFecha();
